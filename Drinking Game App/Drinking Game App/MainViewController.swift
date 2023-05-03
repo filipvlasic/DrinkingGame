74f7e480f9469ea4fb6b1f167c016c01e1d6ac29
filipvlasic <filipvlasic2008@gmail.com>
2023-05-03 19:28:05 +0200

    Inicijalizacija projekta

---
+++ Drinking Game App/Drinking Game App/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Drinking Game App
+//
+//  Created by Filip Vlašić on 03.05.2023..
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
